cb49222cb3c59bbd4145e193a54eda16d6ae315d
pianowithalex <59804251+pianowithalex@users.noreply.github.com>
2020-09-02 19:06:09 +1200

    Hide and go complete, aesthetic changes buttons alligned

--- resources/javascript/mushi.js
+++ resources/javascript/mushi.js
@@ -19,19 +19,20 @@ function rotateMushi(event) {
 }
 
 function addMush(event) {
-    console.log('its working so far')
     event.preventDefault();
-    const mush = mushiForm.mush.value;
-    const newMushLi = document.createElement('li')
-    const newMushAvatar = document.createElement('div')
-    newMushAvatar.className = 'avatar'
-    const newMushText = document.createElement('span')
-    newMushText.innerText = mush
-    const mushes = document.getElementById('mushes').getElementsByTagName('ul')[0]
-    const newerMushLi = mushes.appendChild(newMushLi);
-    newerMushLi.appendChild(newMushAvatar);
-    newerMushLi.appendChild(newMushText)
-    mushiForm.mush.value = "";
+    if (mushiForm.mush.value != ""){
+        const mush = mushiForm.mush.value;
+        const newMushLi = document.createElement('li')
+        const newMushAvatar = document.createElement('div')
+        newMushAvatar.className = 'avatar'
+        const newMushText = document.createElement('span')
+        newMushText.innerText = mush
+        const mushes = document.getElementById('mushes').getElementsByTagName('ul')[0]
+        const newerMushLi = mushes.appendChild(newMushLi);
+        newerMushLi.appendChild(newMushAvatar);
+        newerMushLi.appendChild(newMushText)
+        mushiForm.mush.value = "";
+    }
 }
 
 // helpers
@@ -55,8 +56,8 @@ function rotate(element, event) {
 }
 
 // add event handlers when mouse events are triggered
-//mushiBorder.onmouseenter = toggleMushi;
-//mushiBorder.onmouseleave = toggleMushi;
+mushiBorder.onmouseenter = toggleMushi;
+mushiBorder.onmouseleave = toggleMushi;
 rotateLeftButton.onclick = rotateMushi;
 rotateRightButton.onclick = rotateMushi;
 mushiForm.onsubmit = addMush;
